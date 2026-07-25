89d6c329c1a34530db9caa8d53b7beec9105acea
Paean V <chiomaonuoha86@gmail.com>
2026-07-25 14:33:30 +0100

    Fix site title and header colors for dark theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Paean Verity';
+export const SITE_DESCRIPTION = 'Christian templates, devotionals, and printable resources to help divine truth become the song our whole life sings.';
