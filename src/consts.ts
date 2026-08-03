38cd2f7459ce78bedc8e5a36172a0ed250f31509
Paean V <chiomaonuoha86@gmail.com>
2026-08-03 13:52:00 +0100

    rewrite site title and site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Paean Verity';
-export const SITE_DESCRIPTION = 'Christian templates, devotionals, and printable resources to help divine truth become the song our whole life sings.';
+export const SITE_DESCRIPTION = 'Christian devotionals, coloring books, templates and printable resources to help the truth become the song your whole life sings.';
